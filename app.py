4ce8079188d8270056c2cc6517602304fbc49074
Clone Liu <karta1041337@gmail.com>
2018-03-05 01:16:13 +0800

    Add comments

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 from dataAPI import DataAPI
 from flask import Flask, jsonify
 
+# Start Flask
 app = Flask(__name__)
+# Minimize the response size
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
+# Convert DataAPI to view function
 data_view = DataAPI.as_view('data_api')
 
+# Handle GET at '/datas' with data_view passing data_id with None
 app.add_url_rule(
     '/datas', defaults={'data_id': None}, view_func=data_view, methods=[
         'get',
     ])
 
+# Handle POST at '/data' with data_view
 app.add_url_rule(
     '/data', view_func=data_view, methods=[
         'post',
     ])
 
+# Handle GET, PUT, and DELETE at'/data/<int:data_id>' with data_view
 app.add_url_rule(
     '/data/<int:data_id>',
     view_func=data_view,
@@ -26,46 +32,52 @@ app.add_url_rule(
     ])
 
 
+# Handle HTTP error code 400
 @app.errorhandler(400)
 def bad_request(error):
     return jsonify({
         'error': 'Bad Request',
-    }), 400
+    }), 400  # Return HTTP error code 400
 
 
+# Handle HTTP error code 401
 @app.errorhandler(401)
 def unauthorized(error):
     return jsonify({
         'error': 'Unauthorized',
-    }), 401
+    }), 401  # Return HTTP error code 401
 
 
+# Handle HTTP error code 403
 @app.errorhandler(403)
 def forbidden(error):
     return jsonify({
         'error': 'Forbidden',
-    }), 403
+    }), 403  # Return HTTP error code 403
 
 
+# Handle HTTP error code 404
 @app.errorhandler(404)
 def not_found(error):
     return jsonify({
         'error': 'Not Found',
-    }), 404
+    }), 404  # Return HTTP error code 404
 
 
+# Handle HTTP error code 405
 @app.errorhandler(405)
 def method_not_allowed(error):
     return jsonify({
         'error': 'Method Not Allowed',
-    }), 405
+    }), 405  # Return HTTP error code 405
 
 
+# Handle HTTP error code 406
 @app.errorhandler(406)
 def not_acceptable(error):
     return jsonify({
         'error': 'Not Acceptable',
-    }), 406
+    }), 406  # Return HTTP error code 406
 
 
 if __name__ == '__main__':
--- dataAPI.py
+++ dataAPI.py
@@ -3,15 +3,20 @@ from copy import deepcopy
 from flask import abort, jsonify, request
 from flask.views import MethodView
 
+# Create a empty data set
 datas = []
 
 
+# Create a MethodView class
 class DataAPI(MethodView):
 
+    # Handle GET request with 1 argument named data_id
     def get(self, data_id):
         if data_id is None:
+            # Make a hard copy of datas
             _datas = deepcopy(datas)
             for _data in _datas:
+                # Replace id with URI
                 _data['uri'] = 'http://127.0.0.1:5000/data/' + str(_data['id'])
                 del _data['id']
             return jsonify({
@@ -22,39 +27,55 @@ class DataAPI(MethodView):
             if _data['id'] is data_id:
                 return jsonify({'data': _data})
 
+        # Raise an HTTP 404 error
         abort(404)
 
+    # Handle POST request
     def post(self):
         if request.is_json:
+            # Export JSON request to data
             data = request.json
         else:
+            # Convert form request to dictionary and expoet it to data
             data = request.form.to_dict()
         if data is None:
+            # Raise an HTTP 400 error
             abort(400)
         if 'title' not in data or type(data['title']) is not str:
+            # Raise an HTTP 400 error
             abort(400)
         if 'description' not in data or type(data['description']) is not str:
+            # Raise an HTTP 400 error
             abort(400)
 
+        # Calculate and assign id
         data['id'] = len(datas) + 1
         datas.append(data)
 
+        # Make a hard copy of data
         _data = deepcopy(data)
+        # Replace id with URI
         _data['uri'] = request.base_url + '/' + str(_data['id'])
         del _data['id']
 
-        return jsonify({'data': _data}), 201
+        return jsonify({'data': _data}), 201  # Return HTTP error code 201
 
+    # Handle PUT request with 1 argument named data_id
     def put(self, data_id):
         if request.is_json:
+            # Export JSON request to data
             data = request.json
         else:
+            # Convert form request to dictionary and expoet it to data
             data = request.form.to_dict()
         if data is None:
+            # Raise an HTTP 400 error
             abort(400)
         if 'title' in data and type(data['title']) is not str:
+            # Raise an HTTP 400 error
             abort(400)
         if 'description' in data and type(data['description']) is not str:
+            # Raise an HTTP 400 error
             abort(400)
 
         for _data in datas:
@@ -65,12 +86,15 @@ class DataAPI(MethodView):
                     _data['description'] = data['description']
                 return jsonify({'data': _data})
 
+        # Raise an HTTP 404 error
         abort(404)
 
+    # Handle DELETE request with 1 argument named data_id
     def delete(self, data_id):
         for _data in datas:
             if _data['id'] is data_id:
                 datas.remove(_data)
                 return jsonify({'result': True})
 
+        # Raise an HTTP 404 error
         abort(404)
